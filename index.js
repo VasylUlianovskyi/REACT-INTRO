947b3937725bd756346a9e4a525be3f0639e2770
Vasyl Ulianovskyi <vasia.kit434@gmail.com>
2024-07-06 18:54:03 +0300

    Add news task

--- index.js
+++ index.js
@@ -11,26 +11,10 @@ function App() {
       <Header />
       <main>
         <UserCard />
+        <News />
       </main>
       <Footer />
     </>
-  );
-}
-
-function UserCard() {
-  const user = {
-    name: "Ivo",
-    age: 15,
-    isMale: false,
-    imgSrc:
-      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlciUyMHByb2ZpbGV8ZW58MHx8MHx8fDA%3D&w=1000&q=80",
-  };
-  return (
-    <article className="userCard">
-      <h2 className="userName">{user.name}</h2>
-      <img className="userImg" src={user.imgSrc} alt={user.name} />
-      <p className="userAge">{user.age}</p>
-    </article>
   );
 }
 
@@ -44,6 +28,63 @@ function Header() {
       ></img>
       <h1 className="pageTitle">Page Title</h1>
     </header>
+  );
+}
+
+function UserCard() {
+  const user = {
+    name: "Ivo",
+    age: 19,
+    isMale: false,
+    imgSrc:
+      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlciUyMHByb2ZpbGV8ZW58MHx8MHx8fDA%3D&w=1000&q=80",
+    favoriteColor: "orange",
+  };
+
+  const nameStyle = {
+    color: "red",
+    backgroundColor: "green",
+    border: "3px solid yellow",
+  };
+
+  const imgBorderStyle = {
+    border: `2px solid ${user.favoriteColor}`,
+  };
+
+  const ageStyle = {
+    color: user.age >= 18 ? "blue" : "green",
+  };
+
+  return (
+    <article className="userCard">
+      <h2 className="userName" style={nameStyle}>
+        {user.name}
+      </h2>
+      <img
+        className="userImg"
+        style={imgBorderStyle}
+        src={user.imgSrc}
+        alt={user.name}
+      />
+      <p className="userAge" style={ageStyle}>
+        {user.age}
+      </p>
+    </article>
+  );
+}
+
+function News() {
+  const news = { title: "Some news", body: "Something hapenned", isGood: true };
+
+  const newsStyle = {
+    color: news.isGood ? "green" : "red ",
+  };
+
+  return (
+    <article style={newsStyle}>
+      <h3>{news.title}</h3>
+      <p>{news.body}</p>
+    </article>
   );
 }
 
